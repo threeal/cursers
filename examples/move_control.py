a627d9ff24c6a53da37bc382927597348f62fcbe
Alfi Maulana <alfi.maulana.f@gmail.com>
2025-07-06 21:05:01 +0700

    feat: add lifecycle to the `App` class
    Signed-off-by: Alfi Maulana <alfi.maulana.f@gmail.com>

--- examples/move_control.py
+++ examples/move_control.py
@@ -1,39 +1,48 @@
 import time
 
-from cursers import App
+import cursers
 
 KEY_ESC = 27
 
-if __name__ == "__main__":
-    with App() as app:
-        y = 0
-        x = 0
 
+class MoveControlApp(cursers.App):
+    def __init__(self) -> None:
+        super().__init__()
+        self.y = 0
+        self.x = 0
+
+    def on_enter(self) -> None:
         title = "Movement Control"
-        app.draw_text(0, (29 - len(title)) // 2, title, bold=True)
+        self.draw_text(0, (29 - len(title)) // 2, title, bold=True)
 
-        app.draw_text(3, 2, f"X coordinate: {x:12}")
-        app.draw_text(4, 2, f"Y coordinate: {y:12}")
+        self.draw_text(3, 2, f"X coordinate: {self.x:12}")
+        self.draw_text(4, 2, f"Y coordinate: {self.y:12}")
 
-        app.draw_text(7, 2, "Keyboard Controls:", bold=True)
-        app.draw_text(8, 4, "W/S - Move up/down")
-        app.draw_text(9, 4, "A/D - Move left/right")
-        app.draw_text(10, 4, "ESC - Exit app", bold=True)
+        self.draw_text(7, 2, "Keyboard Controls:", bold=True)
+        self.draw_text(8, 4, "W/S - Move up/down")
+        self.draw_text(9, 4, "A/D - Move left/right")
+        self.draw_text(10, 4, "ESC - Exit app", bold=True)
 
-        while True:
-            key = app.stdscr.getch()
-            if key == KEY_ESC:
-                break
-            elif key == ord("w"):
-                y -= 1
-            elif key == ord("s"):
-                y += 1
-            elif key == ord("a"):
-                x -= 1
-            elif key == ord("d"):
-                x += 1
+    def on_update(self) -> None:
+        key = self.stdscr.getch()
+        if key == KEY_ESC:
+            self.exit()
+            return
+        if key == ord("w"):
+            self.y -= 1
+        elif key == ord("s"):
+            self.y += 1
+        elif key == ord("a"):
+            self.x -= 1
+        elif key == ord("d"):
+            self.x += 1
 
-            app.draw_text(3, 16, f"{x:12}")
-            app.draw_text(4, 16, f"{y:12}")
+        self.draw_text(3, 16, f"{self.x:12}")
+        self.draw_text(4, 16, f"{self.y:12}")
 
+
+if __name__ == "__main__":
+    with MoveControlApp() as app:
+        while app.is_running():
+            app.update()
             time.sleep(1 / 30)
--- src/cursers/app.py
+++ src/cursers/app.py
@@ -3,15 +3,44 @@ from typing import Self
 
 
 class App:
+    def __init__(self) -> None:
+        self._is_running = False
+
     def __enter__(self) -> Self:
         self.stdscr = curses.initscr()
         self.stdscr.nodelay(True)  # noqa: FBT003
         curses.curs_set(0)
         curses.noecho()
+
+        self.on_enter()
+        self._is_running = True
+
         return self
 
     def __exit__(self, *args: object) -> None:
+        self._is_running = False
+        self.on_exit()
         curses.endwin()
+
+    def is_running(self) -> bool:
+        return self._is_running
+
+    def update(self) -> None:
+        if self._is_running:
+            self.on_update()
+            self.stdscr.refresh()
+
+    def exit(self) -> None:
+        self._is_running = False
+
+    def on_enter(self) -> None:
+        pass
+
+    def on_update(self) -> None:
+        pass
+
+    def on_exit(self) -> None:
+        pass
 
     def draw_text(self, y: int, x: int, text: str, *, bold: bool = False) -> None:
         attr = curses.A_BOLD if bold else curses.A_NORMAL
